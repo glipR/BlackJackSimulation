63ccfd3e2ecb78106877e2ffa11894e28771ecd6
Jackson <jgoe0001@student.monash.edu>
2018-05-01 12:44:42 +1000

    Created Deck and begun constants.py

---
+++ src/Deck.py
@@ -0,0 +1,30 @@
+from constants import *
+import random
+
+"""
+Basic Deck implementation
+	52 Cards, each with a suit and face
+	Able to pick with replacement, or without replacement
+"""
+
+class Deck:
+
+	def __init__(self):
+
+		self.current_deck = [None]*(len(SUITS)*len(FACES))
+		for x in range(len(SUITS)):
+			for y in range(len(FACES)):
+				self.current_deck[x*len(FACES) + y] = [SUITS[x], FACES[y]]
+
+
+	def pick_card(self, replacement = False):
+		if len(self.current_deck) == 0:
+			raise ValueError("Deck is empty")
+		index = random.randint(0, len(self.current_deck)-1)
+
+		returning_card =  self.current_deck[index]
+
+		if not replacement:
+			self.current_deck = self.current_deck[:index] + self.current_deck[index+1:]
+
+		return returning_card
---
+++ src/constants.py
@@ -0,0 +1,11 @@
+SUIT = 0
+FACE = 1
+
+SPADE = 0
+HEART = 1
+CLUB = 2
+DIAMOND = 3
+
+SUITS = ["S", "H", "C", "D"]
+
+FACES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
